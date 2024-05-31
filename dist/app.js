dbc246c6d636c7676cf8b0888124ede35bc927f4
BrunoBaia <bruno1397@gmail.com>
2024-05-31 19:24:03 -0300

    update cors

--- dist/app.js
+++ dist/app.js
@@ -39,14 +39,14 @@ class App {
   }
 
   middlewares() {
+    this.app.use(_cors2.default.call(void 0, corsOptions));
+    this.app.use(_helmet2.default.call(void 0, ));
+    this.app.use(_express2.default.urlencoded({ extended: true }));
+    this.app.use(_express2.default.json());
     this.app.use('/images/', (req, res, next) => {
       res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
       next();
     }, _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
-    this.app.use(_helmet2.default.call(void 0, ));
-    this.app.use(_express2.default.urlencoded({ extended: true }));
-    this.app.use(_express2.default.json());
-    this.app.use('/images/', _express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
   }
 
   routes() {
--- src/app.js
+++ src/app.js
@@ -39,14 +39,14 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
     this.app.use('/images/', (req, res, next) => {
       res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
       next();
     }, express.static(resolve(__dirname, '..', 'uploads', 'images')));
-    this.app.use(helmet());
-    this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(express.json());
-    this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
   }
 
   routes() {
