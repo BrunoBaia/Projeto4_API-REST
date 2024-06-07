a336ec97ba343332ab855937e0d4e2f8c7b24eaa
BrunoBaia <bruno1397@gmail.com>
2024-06-07 18:02:53 -0300

    Add express-delay

--- dist/app.js
+++ dist/app.js
@@ -7,6 +7,7 @@ require('./database');
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
+var _expressdelay = require('express-delay'); var _expressdelay2 = _interopRequireDefault(_expressdelay);
 var _path = require('path');
 
 var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
@@ -41,6 +42,7 @@ class App {
   middlewares() {
     this.app.use(_cors2.default.call(void 0, corsOptions));
     this.app.use(_helmet2.default.call(void 0, ));
+    this.app.use(_expressdelay2.default.call(void 0, 2000));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use('/images/', (req, res, next) => {
