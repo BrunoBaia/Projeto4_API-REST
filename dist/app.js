8abb55a3820217c4cca1bec77e45beb00cc07942
BrunoBaia <bruno1397@gmail.com>
2024-06-25 21:18:36 -0300

    Update whitelist

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,7 @@ require('./database');
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
-var _expressdelay = require('express-delay'); var _expressdelay2 = _interopRequireDefault(_expressdelay);
+//import delay from 'express-delay';
 var _path = require('path');
 
 var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
@@ -17,8 +17,8 @@ var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interop
 var _fotoRoutes = require('./routes/fotoRoutes'); var _fotoRoutes2 = _interopRequireDefault(_fotoRoutes);
 
 const whiteList = [
-  "http://34.95.151.145:81",
-  "http://localhost:3000"    //para testes, remover depois
+  "http://34.95.151.145:82",
+  //"http://localhost:3000"    //para testes, remover depois
 ];
 
 const corsOptions = {
@@ -42,7 +42,7 @@ class App {
   middlewares() {
     this.app.use(_cors2.default.call(void 0, corsOptions));
     this.app.use(_helmet2.default.call(void 0, ));
-    this.app.use(_expressdelay2.default.call(void 0, 2000));
+    //this.app.use(delay(2000));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use('/images/', (req, res, next) => {
